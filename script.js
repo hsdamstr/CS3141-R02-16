db0e7902b1d0ff03f63cb71ad927343387861e01
ccchopsticks <99040229+ccchopsticks@users.noreply.github.com>
2022-03-11 17:50:13 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-import Deck from "./deck.js"
-
-const computerCardSlot =document.querySelector('.computer-card-slot')
-
-const deck = new Deck()
-deck.shuffle()
-
-computerCardSlot.appendChild(deck.cards[0].getHTML())
