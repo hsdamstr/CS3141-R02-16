45781d4b047c1955fcb6281288d7872241762b90
ccchopsticks <99040229+ccchopsticks@users.noreply.github.com>
2022-03-16 11:06:43 -0400

    Delete button.js

--- button.js
+++
@@ -1,9 +0,0 @@
-//var theirName= window.prompt("Enter your name");
-
-//console.log("Welcome", theirName);
-
-document.getElementById("myButton").onclick = function(){
-    
-    var name = document.getElementById("myText").value;
-    console.log("Welcome", name);
-}
