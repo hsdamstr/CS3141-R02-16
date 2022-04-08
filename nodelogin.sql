5f08f01967d9e24adc243ab58a121d811f2fdcca
clstrach <99043303+clstrach@users.noreply.github.com>
2022-04-08 11:37:00 -0400

    Add files via upload

--- nodelogin.sql
+++ nodelogin.sql
@@ -5,14 +5,11 @@ CREATE DATABASE IF NOT EXISTS `nodelogin` DEFAULT CHARACTER SET utf8 COLLATE utf
 USE `nodelogin`;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(50) NOT NULL,
-  password varchar(255) NOT NULL,
-  password2 varchar(255) NOT NULL,
-  email varchar(100) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
-
-select * from accounts;
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
