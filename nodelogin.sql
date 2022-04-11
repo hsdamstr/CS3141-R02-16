750f10fbbdcbe21418740fdce4fbab16a04a58a5
clstrach <99043303+clstrach@users.noreply.github.com>
2022-04-11 09:37:01 -0400

    Now including spaces for user stats

--- nodelogin.sql
+++ nodelogin.sql
@@ -1,18 +1,23 @@
 ALTER USER 'root' @'localhost' IDENTIFIED WITH mysql_native_password BY 'teamsoftware16';
 
 
-CREATE DATABASE IF NOT EXISTS `myapp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `nodelogin`;
+CREATE DATABASE IF NOT EXISTS myapp DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE myapp;
  
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+  id int(11) NOT NULL,
+  name varchar(50) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(100) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
  
-INSERT INTO `users` (`id`, `name`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
- 
-ALTER TABLE `users` ADD PRIMARY KEY (`id`);
-ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE users ADD PRIMARY KEY (`id`);
+ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+ALTER TABLE users ADD wins int(5);
+ALTER TABLE users ADD q_time time(0);
+ALTER TABLE users ADD high_score int(10);
+
+select * from users;
+
 
